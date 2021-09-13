860e272d5869bc3f771716d5776e8d7141e26367
Richard madrigar <richard_madrigar99@hotmail.com>
2021-09-13 18:18:14 -0300

    first

---
+++ src/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const pesquisarCep = async () => {
+    limparFormulario(); //funcao para limpar toda vez que digitar alguma coisa 
+
+    const cep = document.getElementById('cep').value //pegando o valor do input do cep
+    const url = `http://viacep.com.br/ws/${cep}/json/`; //passando a variavel para dentro do link
+
+    if (cepValido(cep)) {
+        const dados = await fetch(url) //await aguardando resolver o URL
+        const endereco = await dados.json() //transformando em um JSON
+
+        if (endereco.hasOwnProperty("erro")) { // se a um elemento com o "erro"
+            document.getElementById('endereco').value = "CEP não encontrado" //caso invalido
+
+        } else {
+            preencherFormulario(endereco); //chamando uma funcao e mandando endereço como um argumento
+        }
+    } else {
+        document.getElementById('endereco').value = "CEP não encontrado" //caso invalido
+    }
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //verificando se todos são numeros
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep); //verificando se tem 8 digitos
+
+
+
+//TRABALHANDO COM A DOM 
+const limparFormulario = () => {
+    document.getElementById('endereco').value = ''
+    document.getElementById('bairro').value = ''
+    document.getElementById('cidade').value = ''
+    document.getElementById('estado').value = ''
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro
+    document.getElementById('bairro').value = endereco.bairro
+    document.getElementById('cidade').value = endereco.localidade
+    document.getElementById('estado').value = endereco.uf
+}
+
+document.getElementById('cep') //pegando o id CEP
+    .addEventListener('focusout', pesquisarCep); //add o event ao sair do focus => pesquisarCep()
